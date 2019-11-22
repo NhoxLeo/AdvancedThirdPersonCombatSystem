dada1d4f35b03e9dba98b2fd1c52c3fe1849d70b
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-11-22 13:18:48 +0100

    Added visual element select

--- Assets/_Scripts/PlayerStances.cs
+++ Assets/_Scripts/PlayerStances.cs
@@ -11,6 +11,9 @@ public class PlayerStances : MonoBehaviour
     public Material waterMaterial;
     public Material darkMagic;
 
+    [Header("Player Anim")]
+    public Animator elementAnim;
+
     public enum Stances {water, fire, electric, dark }
 
     public void ChangeStance(Stances stance, PlayerBehaviour pb)
@@ -20,18 +23,22 @@ public class PlayerStances : MonoBehaviour
             case Stances.water:
                 player.material = waterMaterial;
                 pb.comboEquipped = PlayerBehaviour.combos.combo1;
+                elementAnim.SetInteger("Element", 0);
                 break;
             case Stances.fire:
                 player.material = fireMaterial;
                 pb.comboEquipped = PlayerBehaviour.combos.combo2;
+                elementAnim.SetInteger("Element", 1);
                 break;
             case Stances.electric:
                 player.material = electricMaterial;
                 pb.comboEquipped = PlayerBehaviour.combos.combo3;
+                elementAnim.SetInteger("Element", 2);
                 break;
             case Stances.dark:
                 player.material = darkMagic;
                 pb.comboEquipped = PlayerBehaviour.combos.combo4;
+                elementAnim.SetInteger("Element", 3);
                 break;
             default:
                 break;
